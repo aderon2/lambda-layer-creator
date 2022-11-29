b6fdaa51c03ed1fc10276007e176bf499d049601
aderon2 <evandingman@gmail.com>
2022-11-29 17:08:27 -0600

    Add support for multiple dependencies and dynamic version number

--- lambda-layer-creator.py
+++ lambda-layer-creator.py
@@ -3,14 +3,17 @@ import subprocess
 import shutil
 import sys
 
-pythonVersion = 'python3.8'
-dependency = 'requests'
+sysVersion = sys.version_info
+pythonVersion = f'python{sysVersion.major}.{sysVersion.minor}'
+dependencies = ['requests', 'beautifulsoup4']
 layerBaseFolder = 'lambda-layer'
 layerFullPath = f'{layerBaseFolder}/python/lib/{pythonVersion}/site-packages'
+pipInstallCommand = [sys.executable, '-m', 'pip', 'install'] + dependencies + ['--target', layerFullPath]
 
+# Delete previously created lambda layers
 if os.path.isdir(layerBaseFolder):
     shutil.rmtree(layerBaseFolder)
 
 os.makedirs(layerFullPath)
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', dependency, '--target', layerFullPath])
+subprocess.check_call(pipInstallCommand)
 shutil.make_archive(layerBaseFolder, 'zip', layerBaseFolder)
