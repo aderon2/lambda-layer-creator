fc4b0de0d1d79880b521095eaeaad49e308b2f93
aderon2 <evandingman@gmail.com>
2022-11-29 20:11:47 -0600

    Restructure into functions.

--- lambda-layer-creator.py
+++ lambda-layer-creator.py
@@ -3,16 +3,22 @@ import subprocess
 import shutil
 import sys
 
-pythonVersion = f'python{sys.version_info.major}.{sys.version_info.minor}'
-dependencies = ['requests', 'beautifulsoup4'] # Modify this to whatever dependencies you want in the lambda layer
-layerBaseFolder = 'lambda-layer'
-layerFullPath = f'{layerBaseFolder}/python/lib/{pythonVersion}/site-packages'
-pipInstallCommand = [sys.executable, '-m', 'pip', 'install'] + dependencies + ['--target', layerFullPath]
+def createLambdaLayer(dependencies, pythonVersion=f'python{sys.version_info.major}.{sys.version_info.minor}'):
+    layerBaseFolder = 'lambda-layer'
+    layerFullPath = f'{layerBaseFolder}/python/lib/{pythonVersion}/site-packages'
+    pipInstallCommand = [sys.executable, '-m', 'pip', 'install'] + dependencies + ['--target', layerFullPath]
 
-# Delete previously created lambda layers
-if os.path.isdir(layerBaseFolder):
-    shutil.rmtree(layerBaseFolder)
+    # Delete previously created lambda layers
+    if os.path.isdir(layerBaseFolder):
+        shutil.rmtree(layerBaseFolder)
 
-os.makedirs(layerFullPath)
-subprocess.check_call(pipInstallCommand)
-shutil.make_archive(layerBaseFolder, 'zip', layerBaseFolder)
+    os.makedirs(layerFullPath)
+    subprocess.check_call(pipInstallCommand)
+    shutil.make_archive(layerBaseFolder, 'zip', layerBaseFolder)
+
+def main():
+    dependencies = ['requests', 'beautifulsoup4'] # Modify this to whatever dependencies you want in the lambda layer
+    createLambdaLayer(dependencies)
+
+if __name__ == '__main__':
+    main()
